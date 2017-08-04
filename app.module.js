6734d602726ae6c14f5d13cffb621b76e4438198
Barry911 <barryogbonna@gmail.com>
2017-08-04 22:45:59 +0100

    update

--- app.module.js
+++ app.module.js
@@ -1,3 +1,3 @@
 (function(){
-    angular.module("kittron", ['ui.router','homeModule','hotelModule','eventCenterModule','ui.router.title','oc.lazyLoad','ngMaterial','720kb.fx','satellizer','account']);
+    angular.module("kittron", ['ui.router','homeModule','hotelModule','eventCenterModule','ui.router.title','oc.lazyLoad','ngMaterial','satellizer','account']);
 })();
